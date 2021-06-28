374755271019fc51480dcda44eddcb63d8510259
pranav11000 <76476679+pranav11000@users.noreply.github.com>
2021-06-28 12:59:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format :'png',
+    png_quality:90
+    });
+    camera=document.getElementById("camera");
+
+    Webcam.attach('#camera');
+    
+    function take_snapshot()
+    {
+        Webcam.snap(function(data_uri){
+            document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'">';
+        });
+    }
+    console.log('ml5 version:', ml5.version)
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/jXC1y6zTC/model.json',modelLoaded);
+    
+    function modelLoaded() {
+        console.log('model loaded!');
+        }
+
+function check()
+ {
+img = document.getElementById("captured_image");
+classifier.classify(img, gotResult);
+  }
+  function gotResult(error, results)
+  {
+   if(error) {
+   console.error(error)    
+   }  
+   else{
+    console.log(results);
+    speak();
+    if(results.label == "good"){
+        document.getElementById("update_emoji").innerHTML = "&#128077";
+    }
+    if(results.label == "perfect"){
+     document.getElementById("update_emoji").innerHTML = "&#128076";
+ }
+ if(results.label == "sad"){
+    document.getElementById("update_emoji").innerHTML = "&#9996";
+ }
+}
+}
+
+
